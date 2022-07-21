7287a742535d73f3a4784fd71e1c1e7e342747b3
shyam7981 <shyamravulapally@gmail.com>
2022-07-21 17:28:06 +0530

    intial

---
+++ src/academy/learnprogramming/stackschallenge/MainTest.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming.stackschallenge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	public void main() {
+		assertEquals(true,Main.checkForPalindrome("madam"));
+		
+	}
+
+}
